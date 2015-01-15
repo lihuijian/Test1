e37f0a1d54f10269ba833ebedf7bdb294a700d7b
lihuijian <475311582@qq.com>
2015-01-15 15:27:50 +0800

    以前没commit的文件commit
    今天重新添加了新建文本文档

--- GitTest/GitTest/Program.cs
+++ GitTest/GitTest/Program.cs
@@ -5,11 +5,78 @@ using System.Text;
 
 namespace GitTest
 {
+    public delegate void MyDelegate(string message);
+
+    public delegate bool MyDel<T>(T value);
+    class MyClass2
+    {
+         
+    }
+    class MyClass1<T,U>:MyClass<int>,ICloneable
+    {
+        private U u { get; set; }
+        private T t { get; set; }
+        public void MyClass1Method()
+        {
+            Console.WriteLine("MyClass1Method");
+        }
+
+        public object Clone()
+        {
+            return this;
+        }
+    }
+    class MyClass<T>
+    {
+        private T Number { get; set; }
+
+        public void Method(T t)
+        {
+            this.Number = default(T);
+            Console.WriteLine(this.Number);
+        }
+    }
     class Program
     {
+        public static void Methed(string message)
+        {
+             Console.WriteLine(message);
+        }
+
+        public static bool ss(int s)
+        {
+            return true;
+        }
+
         static void Main(string[] args)
         {
-            Console.Write("hello");
+            MyDel<int> sDel=new MyDel<int>(ss);
+            sDel(10);
+            MyClass1<int,string> jjj=new MyClass1<int, string>();
+            jjj.Method(10);
+            jjj.MyClass1Method();
+            var myThis= jjj.Clone();
+             MyClass<int> MyObject=new MyClass<int>();
+             MyObject.Method(10);
+            Console.WriteLine("13"+(char)13+"13");
+            Console.WriteLine("10"+(char)10+"10");
+            while (true)
+            {
+
+
+                var myChar1 = Console.Read();
+                //char myChar1 = Convert.ToChar(myChar);
+                if ((myChar1 >= 65 && myChar1 <= 91) || (myChar1 >= 97 && myChar1 <= 122))
+                {
+                    Console.WriteLine("English");
+                }
+                else
+                {
+                    Console.WriteLine("中文");
+                }
+                Console.WriteLine(myChar1);
+            }
+           
         }
     }
 }
---
+++ GitTest/MyT/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyT
+{
+    /// <summary>
+    /// 泛型类
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    class MyClass<T>
+    {
+        public T Unknow { get; set; }
+
+        public MyClass(T t)
+        {
+            this.Unknow = t;
+        }
+        public void Method(T t1)
+        {
+        }
+    }
+
+    interface IInterface<T>
+    {
+        void Method(T);
+    }
+    internal delegate void MyDel<T>(T t);
+    class Program
+    {
+        public static void TestMethod(int a)
+        {
+        }
+
+        public static void TestMethod1(string s)
+        {
+        }
+
+        static void Main(string[] args)
+        {
+            MyClass<int> myClass=new MyClass<int>(10);//声明一个MyClass对象，T这个位置用int代替
+            MyDel<int> del=new MyDel<int>(TestMethod);//声明一个参数为int类型的MyDel委托
+            MyDel<string> del1=new MyDel<string>(TestMethod1);//声明一个参数为string类型的MyDel委托
+        }
+    }
+}
